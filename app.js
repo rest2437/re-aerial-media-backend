eedb8604d6936194de74754798c1f3abd926af01
rest2437 <nocturnalrd@gmail.com>
2021-10-18 23:55:05 -0700

    added contact form

--- app.js
+++ app.js
@@ -3,20 +3,59 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const passport = require("passport");
+const nodemailer = require("nodemailer");
+const router = express.Router();
 require("./config/passport")(passport);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use("/", router);
 app.use(passport.initialize());
 
 // import models
+const contactEmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "Reaerialmedia1@gmail.com",
+    pass: "Rest2437",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (("error", error)) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
 
 // routes & controllers
 app.get("/", (req, res) => {
   res.json({
     name: "Template",
     message: "Welcome Home",
+  });
+});
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: "Reaerialmedia1@gmail.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
   });
 });
 
